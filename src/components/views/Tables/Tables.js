1e23e431e5873af392863c5abf066a38aa940f04
Kacper Koza <kacperkoza34@gmail.com>
2020-01-16 18:23:45 +0100

    Add Tables component

--- src/components/views/Tables/Tables.js
+++ src/components/views/Tables/Tables.js
@@ -1,18 +1,31 @@
 import React from 'react';
-import styles from './Tables.scss';
+import styles from './Tables.module.scss';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 
-class Tables extends React.Component {
+const Tables = () => {
+  const [ status, changeStatus ] = React.useState(null);
 
-  render(){
-    return(
-      <div className={styles.component}>
-        <h2>
-          Tables view
-        </h2>
-      </div>
-    );
-  }
-}
+  const handleSetTable = (value) => {
+    changeStatus(value);
+  };
+
+  return (
+    <Grid container className={styles.component} spacing={2}>
+      <Grid item xs={12} className={styles.grid}>
+        <Grid container className={styles.tablesWrapper}>
+          {[0, 1, 2, 3, 4, 5].map(value => (
+            <Grid key={value} item>
+              <Paper onClick={(event) => handleSetTable(value)} className={styles.table}>
+                <h4 className={ status === value ? styles.active : '' }>Table {value+1}</h4>
+              </Paper>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </Grid>
+  );
+};
 
 Tables.propTypes = {
 
